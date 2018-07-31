e2e434dc058a08119f224990a39139d81c12535d
Mayrhofer Aemilian <aemilian.mayrhofer@gmail.com>
2018-07-31 14:09:17 +0400

    Added fetch function for Warframe world state with sample result file

---
+++ src/access/warframe-worldState.js
@@ -0,0 +1,22 @@
+import { PLATFORM } from '../constants/warframe'
+
+// warframestat.us
+const get_url = (platform=PLATFORM.PC) => {
+  switch (platform) {
+  case PLATFORM.PC:
+    return 'http://content.warframe.com/dynamic/worldState.php'
+  case PLATFORM.PS4:
+    return 'http://content.warframe.ps4.com/dynamic/worldState.php'
+  case PLATFORM.XBOX1:
+    return 'http://content.warframe.xb1.com/dynamic/worldState.php'
+  default:
+    throw new Error('Invalid Platform', 'Invalid platform provided')
+  }
+}
+
+const fetch_world_state = (platform=PLATFORM.PC) => {
+  const url = get_url(platform)
+  return fetch(url)
+}
+
+export { fetch_world_state }
---
+++ src/constants/warframe.js
@@ -0,0 +1,5 @@
+export const PLATFORM = {
+  PC: 'PC',
+  PS4: 'PS4',
+  XBOX1: 'XBOX1'
+}
